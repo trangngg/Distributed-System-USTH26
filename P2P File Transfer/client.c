1a08ec97c0aa89e1bbb485116c0467791d578689
DangDPH <ddang11114816@gmail.com>
2025-12-15 17:25:14 +0700

    update the server IP and detecting IP in client

--- P2P File Transfer/client.c
+++ P2P File Transfer/client.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #define SERVER_PORT 9000
 #define NODE_PORT   10000
-#define BUF_SIZE    1024
-#define MAX_NAME    256
 
-/* Message types (matches server.c) */
-#define MSG_REGISTER 1
-#define MSG_LOOKUP   2
-#define MSG_REPLY    3
+#define BUF_SIZE 1024
+#define MAX_NAME 256
+#define MAX_FILES 100
+
+#define MSG_REGISTER  1
+#define MSG_LOOKUP    2
+#define MSG_REPLY     3
+#define MSG_LIST      4
+#define MSG_DISCOVERY 5
 
 typedef struct {
     char filename[MAX_NAME];
@@ -24,90 +33,199 @@ typedef struct {
     FileEntry entry;
 } Message;
 
-/* Serve file to another node */
+/* Global to track background process */
+pid_t sharing_pid = 0;
+
+/* Global Variable to store Server IP after discovery */
+char SERVER_IP[INET_ADDRSTRLEN]; 
+
+/* ---------- Detect local LAN IP ---------- */
+void get_local_ip(char *ip) {
+    struct ifaddrs *ifaddr, *ifa;
+    getifaddrs(&ifaddr);
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (!ifa->ifa_addr) continue;
+
+        if (ifa->ifa_addr->sa_family == AF_INET) {
+            struct sockaddr_in *sa = (struct sockaddr_in *)ifa->ifa_addr;
+            const char *addr = inet_ntoa(sa->sin_addr);
+
+            if (strcmp(addr, "127.0.0.1") != 0) {
+                strcpy(ip, addr);
+                break;
+            }
+        }
+    }
+    freeifaddrs(ifaddr);
+}
+
+/* ---------- Auto Discover Server IP ---------- */
+void auto_discover_server() {
+    printf("[Init] Looking for server on LAN...\n");
+
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    
+    int broadcast = 1;
+    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
+
+    struct sockaddr_in broadcast_addr;
+    memset(&broadcast_addr, 0, sizeof(broadcast_addr));
+    broadcast_addr.sin_family = AF_INET;
+    broadcast_addr.sin_port = htons(SERVER_PORT);
+    broadcast_addr.sin_addr.s_addr = inet_addr("255.255.255.255");
+
+    Message msg;
+    msg.type = MSG_DISCOVERY;
+
+    sendto(sock, &msg, sizeof(msg), 0, 
+           (struct sockaddr*)&broadcast_addr, sizeof(broadcast_addr));
+
+    struct sockaddr_in server_response_addr;
+    socklen_t len = sizeof(server_response_addr);
+    Message reply;
+    
+    struct timeval tv = {2, 0};
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+
+    if (recvfrom(sock, &reply, sizeof(reply), 0, 
+                (struct sockaddr*)&server_response_addr, &len) > 0) {
+        strcpy(SERVER_IP, inet_ntoa(server_response_addr.sin_addr));
+        printf("[Init] Server found at: %s\n", SERVER_IP);
+    } else {
+        printf("[Error] Server not found. Make sure server is running.\n");
+        exit(1);
+    }
+    close(sock);
+}
+
+/* ---------- Serve file ---------- */
 void serve_file(const char *filename) {
     int server = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(NODE_PORT);
-    addr.sin_addr.s_addr = INADDR_ANY;
+    int opt = 1;
+    setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    bind(server, (struct sockaddr*)&addr, sizeof(addr));
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(NODE_PORT), /* Using Hardcoded Port */
+        .sin_addr.s_addr = INADDR_ANY
+    };
+
+    if (bind(server, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("[Error] Bind failed");
+        return;
+    }
     listen(server, 5);
 
-    printf("Sharing file: %s\n", filename);
+    printf("Sharing '%s' on port %d...\n", filename, NODE_PORT);
 
     while (1) {
         int client = accept(server, NULL, NULL);
+        if (client < 0) continue;
 
         FILE *fp = fopen(filename, "rb");
-        char buffer[BUF_SIZE];
-        int n;
-
-        while ((n = fread(buffer, 1, BUF_SIZE, fp)) > 0)
-            send(client, buffer, n, 0);
-
-        fclose(fp);
+        if (fp) {
+            char buffer[BUF_SIZE];
+            int n;
+            while ((n = fread(buffer, 1, BUF_SIZE, fp)) > 0)
+                send(client, buffer, n, 0);
+            fclose(fp);
+            printf("\n[Background] File '%s' sent to peer.\n", filename);
+        } else {
+            printf("\n[Error] Could not open file '%s'\n", filename);
+        }
         close(client);
     }
 }
 
-/* Register file with discovery server */
+/* ---------- Register file ---------- */
 void register_file(const char *filename) {
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
 
-    struct sockaddr_in server;
-    server.sin_family = AF_INET;
-    server.sin_port = htons(SERVER_PORT);
-    inet_aton("127.0.0.1", &server.sin_addr);
+    struct sockaddr_in server = {
+        .sin_family = AF_INET,
+        .sin_port = htons(SERVER_PORT)
+    };
+    inet_aton(SERVER_IP, &server.sin_addr);
+
+    char local_ip[INET_ADDRSTRLEN];
+    get_local_ip(local_ip);
 
     Message msg;
     msg.type = MSG_REGISTER;
     strcpy(msg.entry.filename, filename);
-    strcpy(msg.entry.node_ip, "127.0.0.1");
-    msg.entry.node_port = NODE_PORT;
+    strcpy(msg.entry.node_ip, local_ip);
+    msg.entry.node_port = NODE_PORT; /* Using Hardcoded Port */
 
     sendto(sock, &msg, sizeof(msg), 0,
            (struct sockaddr*)&server, sizeof(server));
 
     close(sock);
+    printf("Registered '%s' (%s:%d)\n", filename, local_ip, NODE_PORT);
 }
 
-/* Lookup and download file */
-void download_file(const char *filename) {
+/* ---------- Get list ---------- */
+int get_file_list(FileEntry *list) {
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
 
-    struct sockaddr_in server;
-    server.sin_family = AF_INET;
-    server.sin_port = htons(SERVER_PORT);
-    inet_aton("127.0.0.1", &server.sin_addr);
+    struct sockaddr_in server = {
+        .sin_family = AF_INET,
+        .sin_port = htons(SERVER_PORT)
+    };
+    inet_aton(SERVER_IP, &server.sin_addr);
+
+    struct timeval tv = {2, 0};
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
 
     Message msg;
-    msg.type = MSG_LOOKUP;
-    strcpy(msg.entry.filename, filename);
+    msg.type = MSG_LIST;
 
     sendto(sock, &msg, sizeof(msg), 0,
            (struct sockaddr*)&server, sizeof(server));
 
-    recvfrom(sock, &msg, sizeof(msg), 0, NULL, NULL);
-    close(sock);
+    int count = 0;
+    printf("\n--- Available Files ---\n");
 
-    if (msg.entry.node_port == 0) {
-        printf("File not found\n");
+    while (1) {
+        int n = recvfrom(sock, &msg, sizeof(msg), 0, NULL, NULL);
+        if (n <= 0) break;
+
+        if (msg.type == MSG_REPLY && count < MAX_FILES) {
+            list[count] = msg.entry;
+            printf("%d. %s (%s:%d)\n",
+                   count + 1,
+                   msg.entry.filename,
+                   msg.entry.node_ip,
+                   msg.entry.node_port);
+            count++;
+        }
+    }
+    close(sock);
+    if (count == 0) printf("No files available.\n");
+    return count;
+}
+
+/* ---------- Download ---------- */
+void download_selected_file(FileEntry entry) {
+    int peer = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in peer_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(entry.node_port)
+    };
+    inet_aton(entry.node_ip, &peer_addr.sin_addr);
+
+    if (connect(peer, (struct sockaddr*)&peer_addr, sizeof(peer_addr)) < 0) {
+        printf("[Error] Could not connect to peer %s:%d\n", entry.node_ip, entry.node_port);
+        close(peer);
         return;
     }
 
-    int peer = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in peer_addr;
+    char outname[300];
+    sprintf(outname, "downloaded_%s", entry.filename);
 
-    peer_addr.sin_family = AF_INET;
-    peer_addr.sin_port = htons(msg.entry.node_port);
-    inet_aton(msg.entry.node_ip, &peer_addr.sin_addr);
-
-    connect(peer, (struct sockaddr*)&peer_addr, sizeof(peer_addr));
-
-    FILE *fp = fopen(filename, "wb");
+    FILE *fp = fopen(outname, "wb");
     char buffer[BUF_SIZE];
     int n;
 
@@ -117,26 +235,75 @@ void download_file(const char *filename) {
     fclose(fp);
     close(peer);
 
-    printf("Download complete: %s\n", filename);
+    printf("Downloaded: %s\n", outname);
 }
 
+/* ---------- Main ---------- */
 int main() {
+    /* Auto-Discover Server immediately */
+    auto_discover_server();
+
     int choice;
     char filename[MAX_NAME];
+    FileEntry available[MAX_FILES];
 
-    printf("1. Share file\n2. Download file\nChoice: ");
-    scanf("%d", &choice);
+    while (1) {
+        printf("\n=== P2P File Transfer ===\n");
+        printf("1. Share file\n");
+        printf("2. Download file(s)\n");
+        printf("3. Exit\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
 
-    if (choice == 1) {
-        printf("File name: ");
-        scanf("%s", filename);
-        register_file(filename);
-        serve_file(filename);
-    } else {
-        printf("File name: ");
-        scanf("%s", filename);
-        download_file(filename);
+        if (choice == 1) {
+            if (sharing_pid > 0) {
+                printf("[!] Stopping previous share (PID %d)...\n", sharing_pid);
+                kill(sharing_pid, SIGKILL);
+                waitpid(sharing_pid, NULL, 0);
+                sharing_pid = 0;
+            }
+
+            printf("File name: ");
+            scanf("%s", filename);
+            
+            register_file(filename); /* No port arg */
+
+            pid_t pid = fork();
+
+            if (pid == 0) {
+                serve_file(filename); /* No port arg */
+                exit(0); 
+            }
+            else if (pid > 0) {
+                sharing_pid = pid;
+                printf("[Info] Background server started (PID: %d)\n", pid);
+                usleep(100000); 
+            }
+        }
+        else if (choice == 2) {
+            printf("Show all shared files:\n");
+            int count = get_file_list(available);
+            if (count == 0) continue;
+
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF); 
+
+            char line[256];
+            printf("Select file numbers: ");
+            fgets(line, sizeof(line), stdin);
+
+            char *token = strtok(line, ", ");
+            while (token) {
+                int idx = atoi(token) - 1;
+                if (idx >= 0 && idx < count)
+                    download_selected_file(available[idx]);
+                token = strtok(NULL, ", ");
+            }
+        }
+        else {
+            if (sharing_pid > 0) kill(sharing_pid, SIGKILL);
+            break;
+        }
     }
-
     return 0;
 }
--- P2P File Transfer/server.c
+++ P2P File Transfer/server.c
@@ -2,15 +2,17 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 
 #define SERVER_PORT 9000
 #define MAX_FILES   100
 #define MAX_NAME    256
 
-/* Message types */
-#define MSG_REGISTER 1
-#define MSG_LOOKUP   2
-#define MSG_REPLY    3
+#define MSG_REGISTER  1
+#define MSG_LOOKUP    2
+#define MSG_REPLY     3
+#define MSG_LIST      4
+#define MSG_DISCOVERY 5
 
 typedef struct {
     char filename[MAX_NAME];
@@ -26,52 +28,84 @@ typedef struct {
 FileEntry database[MAX_FILES];
 int file_count = 0;
 
-int main() {
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+int find_file(const char *filename) {
+    for (int i = 0; i < file_count; i++) {
+        if (strcmp(database[i].filename, filename) == 0)
+            return i;
+    }
+    return -1;
+}
 
+int main() {
+    int sock;
     struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+    Message msg;
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) { perror("socket"); return 1; }
+
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-
-    printf("Server running on port %d...\n", SERVER_PORT);
-
-    Message msg;
-    socklen_t addr_len = sizeof(client_addr);
-
-    while (1) {
-        recvfrom(sock, &msg, sizeof(msg), 0,
-                 (struct sockaddr*)&client_addr, &addr_len);
-
-        if (msg.type == MSG_REGISTER) {
-            database[file_count++] = msg.entry;
-
-            printf("Registered: %s from %s:%d\n",
-                   msg.entry.filename,
-                   msg.entry.node_ip,
-                   msg.entry.node_port);
-        }
-
-        else if (msg.type == MSG_LOOKUP) {
-            Message reply;
-            reply.type = MSG_REPLY;
-            memset(&reply.entry, 0, sizeof(FileEntry));
-
-            for (int i = 0; i < file_count; i++) {
-                if (strcmp(database[i].filename,
-                           msg.entry.filename) == 0) {
-                    reply.entry = database[i];
-                    break;
-                }
-            }
-
-            sendto(sock, &reply, sizeof(reply), 0,
-                   (struct sockaddr*)&client_addr, addr_len);
-        }
+    if (bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        close(sock);
+        return 1;
     }
 
+    printf("Discovery Server running on port %d...\n", SERVER_PORT);
+
+    while (1) {
+        ssize_t n = recvfrom(sock, &msg, sizeof(msg), 0,
+                             (struct sockaddr*)&client_addr, &addr_len);
+        if (n <= 0) continue;
+
+        if (msg.type == MSG_REGISTER) {
+            if (file_count >= MAX_FILES) {
+                printf("[ERROR] Server full\n");
+                continue;
+            }
+            if (find_file(msg.entry.filename) == -1) {
+                database[file_count++] = msg.entry;
+                printf("[REGISTER] '%s' from %s:%d\n", msg.entry.filename, msg.entry.node_ip, msg.entry.node_port);
+            } else {
+                printf("[!][File] '%s' already registered\n", msg.entry.filename);
+            }
+        }
+        else if (msg.type == MSG_LOOKUP) {
+            Message reply;
+            memset(&reply, 0, sizeof(reply));
+            reply.type = MSG_REPLY;
+            int index = find_file(msg.entry.filename);
+            if (index != -1) {
+                reply.entry = database[index];
+                printf("[LOOKUP] '%s' found\n", msg.entry.filename);
+            } else {
+                printf("[LOOKUP] '%s' not found\n", msg.entry.filename);
+            }
+            sendto(sock, &reply, sizeof(reply), 0, (struct sockaddr*)&client_addr, addr_len);
+        }
+        else if (msg.type == MSG_LIST) {
+            Message reply;
+            reply.type = MSG_REPLY;
+            printf("LIST request received\n");
+            for (int i = 0; i < file_count; i++) {
+                reply.entry = database[i];
+                sendto(sock, &reply, sizeof(reply), 0, (struct sockaddr*)&client_addr, addr_len);
+            }
+        }
+        /* DISCOVERY HANDLER */
+        else if (msg.type == MSG_DISCOVERY) {
+            printf("[DISCOVERY] Client looking for server from %s\n", inet_ntoa(client_addr.sin_addr));
+            Message reply;
+            memset(&reply, 0, sizeof(reply));
+            reply.type = MSG_REPLY;
+            sendto(sock, &reply, sizeof(reply), 0, (struct sockaddr*)&client_addr, addr_len);
+        }
+    }
     close(sock);
     return 0;
 }
